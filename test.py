bfc327708d1d3c0dc6daf6492a504ea483455dbf
Macint128 <jim257302@gmail.com>
2025-03-12 17:45:06 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title('Hello World!')
-st.image('https://cdn.pixabay.com/photo/2023/11/17/16/54/boat-8394639_1280.jpg', width=200)
+st.image('https://cdn.pixabay.com/photo/2023/11/17/16/54/boat-8394639_1280.jpg', width=10000)
