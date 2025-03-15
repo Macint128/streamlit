44532f7072d8c62eeefd4b2da5180b9964b949da
Macint128 <jim257302@gmail.com>
2025-03-15 16:54:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title('호롤로롤로롤로')
 
 st.write("이건 간단한 웹앱입니다!")
+st.image("https://cdn.pixabay.com/photo/2024/08/21/14/51/four-dimensional-8986442_1280.png")
 
 user_input = st.text_input("이름을 입력하세요:")
 st.write(f"안녕하세요, {user_input}님!")
