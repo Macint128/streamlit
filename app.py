3194b64484304326cbd5dd3f451effb3cf0f2aea
Macint128 <jim257302@gmail.com>
2025-03-15 17:35:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,5 @@ import streamlit as st
 st.title("안녕")
 st.write("이건 간단한 웹패이지야.")
 
-def button():
-    if st.button("버튼을 눌러보세요!"):
-        st.write("버튼이 눌렸어요!")
+if st.button("버튼을 눌러보세요!"):
+    st.write("버튼이 눌렸어요!")
