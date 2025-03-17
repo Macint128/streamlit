aa5578429d10da788f0c24b336596df4f29407c2
Macint128 <jim257302@gmail.com>
2025-03-17 14:46:19 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
+import time
 
 st.title("안녕하세요")
 st.write("이것은 간단한 웹페이지 입니다")
 
-if st.button("눌러봐!"):
-    st.write("버튼이 눌렸습니다")
-else:
-    st.write(" ")
+while True:
+    if st.button("눌러봐"):
+        st.write("버튼이 눌렸습니다")
+        time.sleep(0.5)
+    else:
+        st.write(" ")
